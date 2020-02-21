1138a7602ca098596ff2fa9c75b34e6693beed9a
Steven Hecking <shecking1115@gmail.com>
2020-02-21 14:16:16 -0500

    Editing User and Session Controllers, User and Session Models
    Controllers:
    - Editing index and show methods to User Controller.
    - Editing index and show methods to Session Controller.
    - Beautifying ApplicationController.
    
    Models:
    - Adding has_many to user.rb.
    - Adding belongs_to to session.rb.
    - Beautifying routes.rb.
    
    Reordering line items in change-password.sh

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
-class SessionsController < ApplicationController
-  before_action :set_session, only: [:show, :update, :destroy]
+# frozen_string_literal: true
+
+class SessionsController < ProtectedController
+  before_action :set_session, only: %i[show update destroy]
 
   # GET /sessions
   def index
@@ -39,13 +41,14 @@ class SessionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_session
-      @session = Session.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def session_params
-      params.require(:session).permit(:date, :time, :practice_time, :notes)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_session
+    @session = Session.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def session_params
+    params.require(:session).permit(:date, :time, :practice_time, :notes)
+  end
 end
--- curl-scripts/auth/change-password.sh
+++ curl-scripts/auth/change-password.sh
@@ -3,8 +3,8 @@
 curl "http://localhost:4741/change-password" \
   --include \
   --request PATCH \
-  --header "Authorization: Token token=${TOKEN}" \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "passwords": {
       "old": "'"${OLDPW}"'",
