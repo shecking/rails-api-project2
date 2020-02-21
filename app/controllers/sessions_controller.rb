295678b8aa4bb02321e87e9e72934f8c1368f458
Steven Hecking <shecking1115@gmail.com>
2020-02-21 12:39:25 -0500

    Editing create curl script and session Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  before_action :set_session, only: [:show, :update, :destroy]
+  before_action :set_session, only: %i[show update destroy]
 
   # GET /sessions
   def index
@@ -39,13 +41,14 @@ class SessionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_session
-      @session = Session.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def session_params
-      params.require(:session).permit(:date, :time, :practice_time, :notes)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_session
+    @session = Session.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def session_params
+    params.require(:session).permit(:date, :time, :practice_time, :notes)
+  end
 end
