ee3e49529d78431504958ce0f10f5fd64f4ffcc1
Steven Hecking <shecking1115@gmail.com>
2020-02-25 18:10:55 -0500

    User controller and session serializer edits
    Changed users_controller inherit from OpenReadController
    to users_controller inherit from ProtectedController.
    Added definition for editable attribute in session serializer.

--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 class SessionSerializer < ActiveModel::Serializer
-  attributes :id, :date, :time, :practice_time, :notes, :user
+  attributes :id, :date, :time, :practice_time, :notes, :user, :editable
+
+  def editable
+    scope == object.user
+  end
 end
